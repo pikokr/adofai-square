6188da6541992865a4b898498024df153dcc0246
파랑이 <parangee9706@gmail.com>
2021-05-22 22:29:57 +0900

    feat: spread tile(right direction)

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import path from "path";
 import * as fs from "fs";
 import {ADOFAIParser} from "./utils";
 
-function execute({outfile,startTile,offset,duration,direction,count,overwrite,filename}: {
+function execute({outfile, startTile, offset, duration, direction, count, overwrite, filename}: {
     filename: string,
     overwrite: boolean,
     startTile: number
@@ -12,10 +12,56 @@ function execute({outfile,startTile,offset,duration,direction,count,overwrite,fi
     duration: number
     direction: Direction
     outfile?: string
+    distance: number
 }) {
     const dir = path.join(filename, '..')
-    const level = ADOFAIParser(fs.readFileSync(filename).toString())
-    console.log(level)
+    const file = fs.readFileSync(filename)
+    const level = ADOFAIParser(file.toString())
+    if (overwrite) {
+        fs.writeFileSync(path.join(), 'backup_' + file)
+        outfile = filename
+    }
+    for (let i = 1; i < count; i++) {
+        for (let j = 1; j < 5; j++) {
+            if (direction === Direction.RIGHT) {
+                const tile = startTile + (4 * i) + j - 1
+                const v = .55 * i
+                let res: any
+                switch (j) {
+                    case 1:
+                        res = [v, v * -1]
+                        break
+                    case 2:
+                        res = [v, v]
+                        break
+                    case 3:
+                        res = [v * -1, v]
+                        break
+                    case 4:
+                        res = [v * -1, v * -1]
+                        break
+                }
+                console.log(res)
+                level.actions.push(
+                    {
+                        "floor": startTile - offset,
+                        "eventType": "MoveTrack",
+                        "startTile": [tile, "ThisTile"],
+                        "endTile": [tile, "ThisTile"],
+                        "duration": duration,
+                        "positionOffset": res,
+                        "rotationOffset": 0,
+                        "scale": 100,
+                        "opacity": 100,
+                        "angleOffset": 0,
+                        "ease": "OutBack",
+                        "eventTag": ""
+                    }
+                )
+            }
+        }
+    }
+    fs.writeFileSync(outfile!, JSON.stringify(level))
 }
 
 enum Direction {
@@ -32,7 +78,8 @@ if (process.env.DEV === 'true') {
         duration: 2,
         offset: 4,
         startTile: 4,
-        outfile: 'out.adofai'
+        outfile: 'out.adofai',
+        distance: .55
     })
 } else
     inquirer.prompt([
@@ -47,7 +94,13 @@ if (process.env.DEV === 'true') {
             name: 'overwrite',
             default: false
         },
-        {name: 'outfile', type: 'input', default: () => 'square.adofai', message: '출력 파일을 입력해주세요', when: answers => !answers.overwrite},
+        {
+            name: 'outfile',
+            type: 'input',
+            default: () => 'square.adofai',
+            message: '출력 파일을 입력해주세요',
+            when: answers => !answers.overwrite
+        },
         {
             message: '사각형 시작 타일',
             type: 'number',
@@ -62,6 +115,12 @@ if (process.env.DEV === 'true') {
             message: '사각형을 몇 타일 전에 보여줄까요?',
             type: 'number',
             name: 'offset'
+        },
+        {
+            type: 'number',
+            message: '펴놓은 타일 사이 거리',
+            name: 'distance',
+            default: .55
         },
         {
             message: '사각형을 보여주는 이벤트 길이',
